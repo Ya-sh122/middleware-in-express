acc91a5dbcf2806ecd104dcb9f0f58b72b57fb1a
Yash Patil <ya88287946@gmail.com>
2026-02-17 14:16:18 +0530

    Initialize Express server and add user middleware
    Set up an Express server and added a middleware to handle user information.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('yuvi');
+const express = require('express');
+const app = express();
+
+function addUser(req,res,next){
+  req.user : 'Guest';
+  next();
+}
+
+app.get('/message', addUser, (req,res)=>{
+  res.send(`<h1>Welcome ${req.user}!</h1>`);
+});
+
+app.listen(3000,()=>{console.log('Server is running at port 3000')});
